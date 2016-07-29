7626457bd2fd0a150a9764601775f24ae8a02ddc
SLAPaper <slapaper.pku@gmail.com>
2016-07-29 11:53:49 +0800

    feat: processing regular expression match

--- wco/__init__.py
+++ wco/__init__.py
@@ -8,18 +8,37 @@ import typing as tg
 from multiprocessing import Pool
 import csv
 import pdb
+import re
 
-def word_count(args: tg.Tuple[str, tg.Dict]) -> tg.List:
+def word_count(args: tg.Tuple[str, tg.Dict[str, tg.List]) -> tg.List:
     if DEBUG:
         print(args)
 
     textin = args[0]
-    key = args[1]
+    keyin = args[1]
 
     with open(textin, 'r', encoding='utf_8') as tsvin:
         tsv_reader = csv.reader(tsvin, delimiter='\t')
-        text = [row for row in tsv_reader]
-    return len(text)
+        result = []
+        for row in tsv_reader:
+            id_, string = row.split('\t')
+            is_match = False
+            for type, keys in keyin.items():
+                if isMatch:
+                    break
+
+                if type in ("abbreviation", "regular_expression":
+                    for key in keys:
+                        if re.search(key, string):
+                            is_match = True
+                            break
+                elif type in ("full_name"):
+                    for key in keys:
+                        if re.search(key, string, re.IGNORECASE):
+                            is_match = True
+                            break
+            result.append((id_, is_match))
+    return result
     
 DEBUG = True
 
