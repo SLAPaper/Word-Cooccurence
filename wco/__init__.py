f3781a6315f0385e37b168689e92fc5d43082af8
SLAPaper <slapaper.pku@gmail.com>
2016-08-01 11:37:37 +0800

    fix: fix syntax error

--- wco/__init__.py
+++ wco/__init__.py
@@ -10,38 +10,32 @@ import csv
 import pdb
 import re
 
-def word_count(args: tg.Tuple[str, tg.Dict[str, tg.List]) -> tg.List:
+def word_count(args: tg.Tuple[str, tg.Dict[str, tg.List]]) -> tg.List:
     """function that count the number of defined keys in given text
-    
     using tuple to store args due to Pool's map method only support one parameter
     """
     if DEBUG:
         print(args)
 
-    textin = args[0]
-    keyin = args[1]
+    row, keyin = args
+    result = []
+    id_, string = row.split('\t')
+    is_match = False
+    for type, keys in keys.items():
+        if isMatch:
+            break
 
-    with open(textin, 'r', encoding='utf_8') as tsvin:
-        tsv_reader = csv.reader(tsvin, delimiter='\t')
-        result = []
-        for row in tsv_reader:
-            id_, string = row.split('\t')
-            is_match = False
-            for type, keys in keyin.items():
-                if isMatch:
+        if type in ("abbreviation", "regular_expression"):
+            for key in keyin.items:
+                if re.search(key, string):
+                    is_match = True
                     break
-
-                if type in ("abbreviation", "regular_expression":
-                    for key in keys:
-                        if re.search(key, string):
-                            is_match = True
-                            break
-                elif type in ("full_name"):
-                    for key in keys:
-                        if re.search(key, string, re.IGNORECASE):
-                            is_match = True
-                            break
-            result.append((id_, is_match))
+        elif type in ("full_name"):
+            for key in keys:
+                if re.search(key, string, re.IGNORECASE):
+                    is_match = True
+                    break
+        result.append((id_, is_match))
     return result
     
 DEBUG = True
@@ -59,6 +53,8 @@ if __name__ == "__main__":
             with open(sys.argv[2], 'r', encoding='utf_8') as keyin:
                 keys = json.load(keyin)
 
-    with Pool(8) as p:
-        result = p.imap(word_count, ((textin, item) for item in keys.items()), 10)
-        print([item for item in result])
+    with open(textin, 'r', encoding='utf_8') as tsvin:
+        tsv_reader = csv.reader(tsvin, delimiter='\t')
+        with Pool(8) as p:
+            result = p.imap(word_count, ((row, keys) for row in tsv_reader), 100)
+            print([item for item in result])
