c950d5dbfe4053830b440f14f9739393682db8c1
SLAPaper <slapaper.pku@gmail.com>
2016-08-04 14:22:13 +0800

    docs: add status notification

--- wco/__init__.py
+++ wco/__init__.py
@@ -11,7 +11,7 @@ import pdb
 import re
 from collections import defaultdict
 
-def word_count(args: tg.Tuple[tg.List[str], tg.Dict[str, tg.List]]) -> tg.Tuple[str, tg.Dict[str, str]]:
+def word_count(args: tg.Tuple[tg.List[str], tg.Dict[str, tg.List]]) -> tg.Tuple[str, str, tg.Dict[str, str]]:
     """function that count the number of defined keys in given text
     using tuple to store args due to Pool's map method only support one parameter
     """
@@ -43,9 +43,12 @@ def word_count(args: tg.Tuple[tg.List[str], tg.Dict[str, tg.List]]) -> tg.Tuple[
         if is_match:
             result[entity] = match.group()
 
-    return id_, result # tg.Dict[str, str]
+    return id_, string, result # tg.Dict[str, str]
     
-DEBUG = True
+DEBUG = False
+NUM_PROCESS = 8
+CHUNK_SIZE = 100
+COOCCURENCE_THRESHOLD = 1
 
 if __name__ == "__main__":
     if DEBUG:
@@ -61,8 +64,18 @@ if __name__ == "__main__":
             with open(sys.argv[2], 'r', encoding='utf_8') as keyin:
                 keys = json.load(keyin)
 
+    print("Config loaded.")
+
     with open(text_file, 'r', encoding='utf_8') as tsvin:
+        print("Text opened, processing.")
         tsv_reader = csv.reader(tsvin, delimiter='\t')
-        with Pool(8) as p:
-            result = p.imap_unordered(word_count, ((row, keys) for row in tsv_reader), 100)
-            print([item for item in result if len(item[1]) > 1])
+        with Pool(NUM_PROCESS) as p:
+            result = p.imap_unordered(word_count, ((row, keys) for row in tsv_reader), CHUNK_SIZE)
+            with open("out_1.tsv", 'w', encoding='utf_8', newline='') as outfile1, open("out_2.tsv", 'w', encoding='utf_8', newline='') as outfile2:
+                tsv_writer1 = csv.writer(outfile1, delimiter='\t')
+                tsv_writer2 = csv.writer(outfile2, delimiter='\t')
+                for id_, original_string, result_dict in result:
+                    if len(result_dict) > COOCCURENCE_THRESHOLD:
+                        tsv_writer1.writerows(([id_, entity, raw_mention] for entity, raw_mention in result_dict.items()))
+                        tsv_writer2.writerow([id_, original_string] + [entity for entity in result_dict])
+        print("Finished. Check out_1.tsv and out_2.tsv file for result")
